4513bce4c8d4bf5cef6bbbaa82de7de8269ec6c2
Backup158 <beekoop158+gh@gmail.com>
2024-11-27 16:36:53 -0600

    Initial commit
    removes read from disk, waiting for other players, and waiting for dedicated server

---
+++ RemoveLoadingStatus/scripts/mods/RemoveLoadingStatus/RemoveLoadingStatus.lua
@@ -0,0 +1,54 @@
+local mod = get_mod("RemoveLoadingStatus")
+local WTL = get_mod("WhatTheLocalization")
+
+mod.localization_templates = {
+	{	
+		id = "ReadFromDisk",
+		loc_keys = {"loc_wait_reason_read_from_disk",},
+		locales = {"en",},
+		handle_func = function(locale, value)
+			return "" 
+		end,
+	},
+    {	
+		id = "DedicatedServer",
+		loc_keys = {"loc_wait_reason_dedicated_server",},
+		locales = {"en",},
+		handle_func = function(locale, value)
+			return "" 
+		end,
+	},
+    {	
+		id = "OtherPlayer",
+		loc_keys = {"loc_wait_reason_other_player",},
+		locales = {"en",},
+		handle_func = function(locale, value)
+			return "" 
+		end,
+	},
+
+    --[[
+    -- TEMPLATE
+	{	
+		id = "",
+		loc_keys = {"loc_",},
+		locales = {"en",},
+		handle_func = function(locale, value)
+			return "" 
+		end,
+	},
+    ]]
+}
+
+-- tell WTL to reload while toggling this bundle
+function mod.on_enabled()
+    if WTL then
+        WTL.reload_templates()
+    end
+end
+
+function mod.on_disabled()
+    if WTL then
+        WTL.reload_templates()
+    end
+end
