ba786ed573a69d996c0665c4fb7dfafb9167887a
Backup158 <beekoop158+gh@gmail.com>
2024-11-27 17:33:24 -0600

    initial test to migrate away from WTL
    thanks raindish

--- RemoveLoadingStatus/scripts/mods/RemoveLoadingStatus/RemoveLoadingStatus.lua
+++ RemoveLoadingStatus/scripts/mods/RemoveLoadingStatus/RemoveLoadingStatus.lua
@@ -1,84 +1,17 @@
 local mod = get_mod("RemoveLoadingStatus")
-local WTL = get_mod("WhatTheLocalization")
 
-mod.localization_templates = {
-	{	
-		id = "ReadFromDisk",
-		loc_keys = {"loc_wait_reason_read_from_disk",},
-		locales = {"en",},
-		handle_func = function(locale, value)
-            -- "READING DATA FROM DISK"
-			return "" 
-		end,
-	},
-    {	
-		id = "DedicatedServer",
-		loc_keys = {"loc_wait_reason_dedicated_server",},
-		locales = {"en",},
-		handle_func = function(locale, value)
-            -- ""
-			return "" 
-		end,
-	},
-    {	
-		id = "OtherPlayer",
-		loc_keys = {"loc_wait_reason_other_player",},
-		locales = {"en",},
-		handle_func = function(locale, value)
-            -- "WAITING FOR OTHER PLAYER(S)"
-			return "" 
-		end,
-	},
-    {	
-		id = "ReadFromDisk",
-		loc_keys = {"loc_wait_reason_read_from_disk",},
-		locales = {"en",},
-		handle_func = function(locale, value)
-            -- "READING DATA FROM DISK"
-			return "" 
-		end,
-	},
-    {	
-		id = "Backend",
-		loc_keys = {"loc_wait_reason_backend",},
-		locales = {"en",},
-		handle_func = function(locale, value)
-            -- "COMMUNICATING WITH FATSHARK BACKEND"
-			return "" 
-		end,
-	},
-    {	
-		id = "Store",
-		loc_keys = {"loc_wait_reason_store",},
-		locales = {"en",},
-		handle_func = function(locale, value)
-            -- 
-			return "" 
-		end,
-	},
+mod:add_global_localize_strings({
+  loc_wait_reason_read_from_disk = {
+    en = ""
+  },
+  loc_wait_reason_dedicated_server = {
+    en = ""
+  },
+  loc_wait_reason_other_backend = {
+    en = ""
+  },
+  loc_wait_reason_other_store = {
+    en = ""
+  },
+})
 
-    --[[
-    -- TEMPLATE
-	{	
-		id = "",
-		loc_keys = {"loc_",},
-		locales = {"en",},
-		handle_func = function(locale, value)
-			return "" 
-		end,
-	},
-    ]]
-}
-
--- tell WTL to reload while toggling this bundle
-function mod.on_enabled()
-    if WTL then
-        WTL.reload_templates()
-    end
-end
-
-function mod.on_disabled()
-    if WTL then
-        WTL.reload_templates()
-    end
-end
