c113304b2a043a82c86c49ac411ca6e208232044
Backup158 <beekoop158+gh@gmail.com>
2024-11-27 19:36:26 -0600

    added other messages
    and fixed the numerous typos i made (SOMEHOW! IT WAS LITERALLY 4 LINES AND 2 WERE WRONG, WITH THE FIFTH LITERALLY BEING MISSING?!?!?!?!)

--- RemoveLoadingStatus/scripts/mods/RemoveLoadingStatus/RemoveLoadingStatus.lua
+++ RemoveLoadingStatus/scripts/mods/RemoveLoadingStatus/RemoveLoadingStatus.lua
@@ -1,16 +1,19 @@
 local mod = get_mod("RemoveLoadingStatus")
 
 mod:add_global_localize_strings({
-  loc_wait_reason_read_from_disk = {
+  loc_wait_reason_backend = {
     en = ""
   },
   loc_wait_reason_dedicated_server = {
     en = ""
   },
-  loc_wait_reason_other_backend = {
+  loc_wait_reason_other_player = {
     en = ""
   },
-  loc_wait_reason_other_store = {
+  loc_wait_reason_read_from_disk = {
+    en = ""
+  },
+  loc_wait_reason_store = {
     en = ""
   },
 })
