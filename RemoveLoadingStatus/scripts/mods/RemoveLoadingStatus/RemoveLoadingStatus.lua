048ede7aa34ab8abf3df2e40fd0b88e9bad0337b
Backup158 <beekoop158+gh@gmail.com>
2024-11-27 20:30:47 -0600

    added platform messages

--- RemoveLoadingStatus/scripts/mods/RemoveLoadingStatus/RemoveLoadingStatus.lua
+++ RemoveLoadingStatus/scripts/mods/RemoveLoadingStatus/RemoveLoadingStatus.lua
@@ -2,18 +2,30 @@ local mod = get_mod("RemoveLoadingStatus")
 
 mod:add_global_localize_strings({
   loc_wait_reason_backend = {
+    -- communicating with fatshark backend
     en = ""
   },
   loc_wait_reason_dedicated_server = {
     en = ""
   },
   loc_wait_reason_other_player = {
+    -- waiting for other player(s)
     en = ""
   },
   loc_wait_reason_read_from_disk = {
+    -- reading data from disk
     en = ""
   },
   loc_wait_reason_store = {
+    en = ""
+  },
+  loc_wait_reason_platform_xbox_live = {
+    en = ""
+  },
+  loc_wait_reason_platform_psn = {
+    en = ""
+  },
+  loc_wait_reason_platform_steam = {
     en = ""
   },
 })
